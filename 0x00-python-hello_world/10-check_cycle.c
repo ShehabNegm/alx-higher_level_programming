336f1b5a3bf05bdec0ce5070d5889011c0941fa2
ShehabNegm <shebonegm@gmail.com>
2023-05-08 14:51:39 -0700

    c function to detect the loop in a singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * check_cycle - will check if linked list has a cycle
+  * @list: head of the list
+  * Return: will return 1 if there is a cycle 0 if not
+  */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *frist = list;
+	listint_t *second = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (frist && second && frist->next)
+	{
+		frist = frist->next;
+		second = second->next->next;
+		if (frist == second)
+			return (1);
+	}
+	return (0);
+}
