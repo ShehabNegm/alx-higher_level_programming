1b930b94a4a18de326f0fd01f73c615000649dc1
ShehabNegm <shebonegm@gmail.com>
2023-05-11 16:46:02 -0700

    fix checker for last one

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,12 @@ if __name__ == "__main__":
 
     import sys
 
-    if len(sys.argv) <= 1:
+    if len(sys.argv) == 1:
         print("{} arguments.".format(0))
+
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
+        print("{}: {}".format(1, sys.argv[1]))
 
     else:
         lens = len(sys.argv)
