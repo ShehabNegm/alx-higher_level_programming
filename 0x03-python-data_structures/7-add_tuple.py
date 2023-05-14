3baf32b76e919b04c27c51f358103daf164d7e82
ShehabNegm <shebonegm@gmail.com>
2023-05-14 12:15:02 -0700

    rewrote the whole logic to add two tuples with python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,20 +2,21 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     """function to retutn a tuple of sum of 2 tuples"""
 
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    La = [0, 0]
+    Lb = [0, 0]
 
-    elif len(tuple_a) == 0:
-        return tuple_b
+    if len(tuple_a) == 1:
+        La[0] = tuple_a[0]
 
-    elif len(tuple_b) == 0:
-        return tuple_a
+    if len(tuple_a) >= 2:
+        La[0] = tuple_a[0]
+        La[1] = tuple_a[1]
 
-    elif len(tuple_a) == 0 and len(tuple_b) == 0:
-        return (0, 0)
+    if len(tuple_b) == 1:
+        Lb[0] = tuple_b[0]
 
-    elif len(tuple_a) == 1 and len(tuple_b) >= 2:
-        return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    if len(tuple_b) >= 2:
+        Lb[0] = tuple_b[0]
+        Lb[1] = tuple_b[1]
 
-    elif len(tuple_a) >= 2 and len(tuple_b) == 1:
-        return (tuple_a[0] + tuple_b[0], 0 + tuple_a[1])
+    return (La[0] + Lb[0], La[1] + Lb[1])
