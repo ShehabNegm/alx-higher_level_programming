c354a553dd19070ebff6534647e67bcbe2d911d3
ShehabNegm <shebonegm@gmail.com>
2023-05-14 00:08:17 -0700

    try to fix checker

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,6 +11,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 0:
         return tuple_a
 
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
+
     elif len(tuple_a) == 1 and len(tuple_b) >= 2:
         return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
 
