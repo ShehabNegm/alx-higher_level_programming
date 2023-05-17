f97c4324b0bb564bdb49b279e3cf0f54fe1fef2b
ShehabNegm <shebonegm@gmail.com>
2023-05-17 14:18:02 -0700

    function to update a kex of a dict

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """function to update dict with keys and values"""
+
+    a_dictionary[key] = value
+    return a_dictionary
