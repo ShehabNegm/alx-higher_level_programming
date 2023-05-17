eb1d7499b0a6f773234fc1c4a9b86744dc1b41b0
ShehabNegm <shebonegm@gmail.com>
2023-05-17 13:21:26 -0700

    check why checks are failing

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     """function to replace element search in my_list by replace"""
 
-    if my_list:
+    if my_list and replace:
 
         return [replace if i == search else i for i in my_list]
