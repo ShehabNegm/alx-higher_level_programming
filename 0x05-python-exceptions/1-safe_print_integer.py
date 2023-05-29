f2661500d14fe11c976dcdad881fce22dafff00f
ShehabNegm <shebonegm@gmail.com>
2023-05-29 12:38:32 -0700

    function to print an integer safely

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """function to print an integer safely"""
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
