baf2ca6d0846bb801438feed11765410c0cf5c69
ShehabNegm <shebonegm@gmail.com>
2023-05-30 12:32:46 -0700

    define a class private arrtribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """class square created"""
+
+    def __init__(self, size):
+        """Initialize square with artibutes size
+        Args:
+            size (int): is a size of the new square
+        """
+
+        self.__size = size
