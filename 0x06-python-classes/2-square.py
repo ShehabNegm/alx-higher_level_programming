ebe430596869e922a9e3b47f6d8f66ddceba0d98
ShehabNegm <shebonegm@gmail.com>
2023-05-30 12:51:08 -0700

    create class and new arrtibutes and raise error if occured

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 2-square.py
+"""create a square class and check its arrtibutes"""
+
+
+class Square:
+    """create a new class Square"""
+
+    def __init__(self, size=0):
+        """initialize a new instance with its size set to zero
+        Args:
+            size (int): set to zero as default
+        Return:
+            raise TypeError if size is not int
+            raiseValue error if size is less than zero
+        """
+        self.__size = size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
