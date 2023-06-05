6d72c08e109f86e96103ae5aa97a7ccd351ff59c
ShehabNegm <shebonegm@gmail.com>
2023-06-05 13:46:25 -0700

    add area and perimeter public instance method to class rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""define a new class"""
+
+
+class Rectangle:
+    """new class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """initialize a new instance of class Rectangle
+        Args:
+            width (int) : width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """function to return the area of rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """function to return the perimeter of a rectangle"""
+        return 2 * (self.__height + self.__width)
