9f9fda8485d945ba104f0af2248e8f4c476b5d76
ShehabNegm <shebonegm@gmail.com>
2023-06-12 14:45:17 -0700

    class basegeometery function to print area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""class base_geometry definition"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """function to return area"""
+
+        raise Exception("area() is not implemented")
