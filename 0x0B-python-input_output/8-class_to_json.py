3aea3c879c90fbe2ba2a3f176c1c3fe18d356d65
ShehabNegm <shebonegm@gmail.com>
2023-06-13 15:35:15 -0700

    fix checker

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
 """function to returns the dictionary description for JSON"""
 
 
 def class_to_json(obj):
     """function to return json rep of an object"""
 
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
