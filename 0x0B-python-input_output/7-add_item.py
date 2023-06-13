4b7c832b7e927511c128e83a954edc4f9f64ac10
ShehabNegm <shebonegm@gmail.com>
2023-06-13 15:13:17 -0700

    handle fix exceptions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-    items_list = load_from_json_file("add_item.json")
+    try:
+        items_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items_list = []
     items_list += argv[1:]
     save_to_json_file(items_list, "add_item.json")
