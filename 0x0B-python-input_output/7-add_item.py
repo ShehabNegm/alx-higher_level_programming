482c135414434017628cce4feae55e52d3eb3434
ShehabNegm <shebonegm@gmail.com>
2023-06-13 15:09:39 -0700

    fix checkers

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,12 @@ import json
 from sys import argv
 
 
-"""script that adds all arguments to a Python list & save them to a file"""
+if __name__ == "__main__":
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-items_list = load_from_json_file("add_item.json")
-items_list += argv[1:]
-save_to_json_file(items_list, "add_item.json")
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    items_list = load_from_json_file("add_item.json")
+    items_list += argv[1:]
+    save_to_json_file(items_list, "add_item.json")
