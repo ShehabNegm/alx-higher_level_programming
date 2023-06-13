a0103e74ac370d1bc030505f71830421031b166e
ShehabNegm <shebonegm@gmail.com>
2023-06-13 16:42:39 -0700

    update class artibutes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""define a class student"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """class instanation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return dictionary rep. of class"""
+
+        if type(attrs) is not list:
+            return self.__dict__
+
+        else:
+            dic = {}
+            if type(attrs) is list:
+                for i in attrs:
+
+                    if type(i) is not str:
+                        return self.__dict__
+
+                    else:
+                        for k in self.__dict__.keys():
+                            if k == i:
+                                dic[i] = self.__dict__[i]
+                return dic
+
+    def reload_from_json(self, json):
+        """function to update class artibutes"""
+        for k, v in json.items():
+            setattr(self, k, v)
