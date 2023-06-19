83a778d92c320b2469ec480bd1788818cb1afa30
ShehabNegm <shebonegm@gmail.com>
2023-06-19 11:40:18 -0700

    add to jason class method tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,9 @@
 
 import unittest
 from models.base import Base
+from io import StringIO
+from unittest.mock import patch
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -48,3 +51,24 @@ class TestBase(unittest.TestCase):
         """test id as string"""
         b_str = Base("shehab")
         self.assertEqual(b_str.id, "shehab")
+
+
+class testBase_to_json_string(unittest.TestCase):
+    """test for to_json_string class method"""
+
+    def test_None_input(self):
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_empty_list(self):
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_normal(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            r1 = Rectangle(10, 7, 2, 8)
+            dictionary = r1.to_dictionary()
+            json_dictionary = Base.to_json_string([dictionary])
+            print(json_dictionary)
+            output = "{}".format(json_dictionary)
+            self.assertEqual(f.getvalue().strip(), output)
