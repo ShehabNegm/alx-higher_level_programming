089ce0e7ad6b8c844c55ef10ba6856640477ca56
ShehabNegm <shebonegm@gmail.com>
2023-06-19 11:15:29 -0700

    update base class by adding json string rep class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # base.py
 """class base"""
+import json
 
 
 class Base:
@@ -17,3 +18,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list of dictionaries"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        else:
+            return json.dumps(list_dictionaries)
