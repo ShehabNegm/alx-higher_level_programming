c71593484cd7a2c934a78f8ec504c036926dedaa
ShehabNegm <shebonegm@gmail.com>
2023-07-18 17:24:19 -0700

    script to show students with score more than 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table
+-- in ascdening order using name and score where score is >= 10
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
