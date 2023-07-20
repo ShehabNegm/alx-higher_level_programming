d6dd7d8578f6acf6b92184cfecb20c506bd190db
ShehabNegm <shebonegm@gmail.com>
2023-07-20 15:33:57 -0700

    fix errors on task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,11 @@
 -- create a database and a new user
 CREATE DATABASE
-	IF NOT EXISTS hbtn_0d_2;
-
+	IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
 	IF NOT EXISTS 'user_0d_2@localhost'
 	IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-	on hbtn_0d_2.*
+	on `hbtn_0d_2.*`
 	to user_0d_2@localhost;
+FLUSH PRIVILEGES;
 
