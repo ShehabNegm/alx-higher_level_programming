b8fa52cd504053d64f316f3b0cea45c7ac087381
ShehabNegm <shebonegm@gmail.com>
2023-07-20 18:57:15 -0700

    nner join two tables using the genere name

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- list all the comedy series in a database
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+on tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+on tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title
