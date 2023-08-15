0b925baaef3a66afb6e38b3036ede469b0f4ce0a
ShehabNegm <shebonegm@gmail.com>
2023-08-14 17:44:48 -0700

    fix error

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ if (process.argv[2] === undefined || isNaN(+process.argv[2])) {
   console.log('Missing size');
 } else if (process.argv.length === 3 && +process.argv[2] > 0) {
   const num = process.argv[2];
-  const sqr = 'x';
+  const sqr = 'X';
   const str = sqr.repeat(+num);
   for (let i = 0; i < +num; i++) {
     console.log(str);
