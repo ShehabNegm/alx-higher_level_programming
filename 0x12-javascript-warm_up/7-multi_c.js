967e531e841451bd858ac2c6117b185f407b9170
ShehabNegm <shebonegm@gmail.com>
2023-08-14 17:24:26 -0700

    fix errors

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+if (process.argv[2] === undefined || isNaN(+process.argv[2])) {
   console.log('Missing number of occurrences');
 } else if (process.argv.length === 3) {
   const num = process.argv[2];
-  if (isNaN(+num)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < +num; i++) {
-      console.log('C is fun');
-    }
+  for (let i = 0; i < +num; i++) {
+    console.log('C is fun');
   }
 }
