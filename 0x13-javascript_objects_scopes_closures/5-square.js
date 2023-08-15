81113b44d5630a46eeb92e59e4198f1337a0768c
ShehabNegm <shebonegm@gmail.com>
2023-08-15 16:23:31 -0700

    add a new subclass square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const sqr = 'X';
+    const str = sqr.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
