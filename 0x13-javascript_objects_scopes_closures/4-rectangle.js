a66017fbbb0959f7060465fe30e2ab1b55950548
ShehabNegm <shebonegm@gmail.com>
2023-08-15 16:08:38 -0700

    add more class methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const sqr = 'X';
+    const str = sqr.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
