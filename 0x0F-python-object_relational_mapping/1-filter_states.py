4ece02706a00fdb9666e3d3b04e61fb2be559a5f
ShehabNegm <shebonegm@gmail.com>
2023-08-19 15:51:39 -0700

    fix output error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,4 +12,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
+
+    db.close()
