33f73b3f24286c89b80e923ca9d61da4dc673fa6
ShehabNegm <shebonegm@gmail.com>
2023-08-19 16:05:03 -0700

    select a row based on user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""python script to connect to db using MySQLdb"""
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * from states WHERE name = '{}'".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
