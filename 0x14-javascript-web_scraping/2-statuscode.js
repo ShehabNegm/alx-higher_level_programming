c3edc3b6971251d6a8a3ec70896cbaef46c343b9
ShehabNegm <shebonegm@gmail.com>
2023-09-28 02:48:18 +0200

    using request modeule for js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, response) {
+  if (err) {
+    console.error(err);
+  }
+  console.log('Code:', response && response.statusCode);
+});
